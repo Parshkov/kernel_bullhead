0026deab78ab345aedc77bff52453efae3d3fc9a
Joe Maples <joe@frap129.org>
2016-07-11 23:15:33 +0000

    cpufreq: chill: Major cleanup, move changes from governor.h to chill.h
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- drivers/cpufreq/cpufreq_governor.h
+++ drivers/cpufreq/cpufreq_governor.h
@@ -208,9 +208,6 @@ struct cs_dbs_tuners {
 	unsigned int min_load;
 	unsigned int twostep_counter;
 	u64 twostep_time;
-	unsigned int sleep_death;
-	unsigned int boost_enabled;
-	unsigned int boost_count;
 };
 
 struct ex_dbs_tuners {
